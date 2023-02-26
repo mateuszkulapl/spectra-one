98380a3631c77187fef07e893122ac1ed4baf993
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-02-26 17:26:58 +0530

    fix : unwated class attribute added

--- inc/extensions/responsive.php
+++ inc/extensions/responsive.php
@@ -25,15 +25,15 @@ function render_responsive_blocks( string $block_content, array $block ) {
 	$responsive_classes = '';
 
 	if ( isset( $block['attrs']['UAGHideDesktop'] ) && true === $block['attrs']['UAGHideDesktop'] ) {
-		$responsive_classes .= ' swt-hide-desktop';
+		$responsive_classes .= 'swt-hide-desktop';
 	}
 
 	if ( isset( $block['attrs']['UAGHideTab'] ) && true === $block['attrs']['UAGHideTab'] ) {
-		$responsive_classes .= ' swt-hide-tablet';
+		$responsive_classes .= 'swt-hide-tablet';
 	}
 
 	if ( isset( $block['attrs']['UAGHideMob'] ) && true === $block['attrs']['UAGHideMob'] ) {
-		$responsive_classes .= ' swt-hide-mobile';
+		$responsive_classes .= 'swt-hide-mobile';
 	}
 
 	$dom        = dom( $block_content );
@@ -44,9 +44,12 @@ function render_responsive_blocks( string $block_content, array $block ) {
 	}
 
 	$classes = $first_item->getAttribute( 'class' );
-	$first_item->setAttribute( 'class', $classes . $responsive_classes );
+	$add_extra_space = $classes ? ' ' : '';
 
-	$block_content = $dom->saveHTML();
+	if( $responsive_classes ) {
+		$first_item->setAttribute( 'class', $classes . $add_extra_space . $responsive_classes );
+		$block_content = $dom->saveHTML();
+	}
 
 	add_filter( 'swt_dynamic_theme_css', SWT_NS . 'responsive_blocks_inline_css' );
 
