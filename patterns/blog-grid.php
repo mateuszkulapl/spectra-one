8415b2aad9b84159cc8ebf100d058bd58ee1bb48
Roshan Adhikari <roshan.adhikari.se@gmail.com>
2023-03-09 06:45:09 +0545

    SPEC-46 Fixed spacing issue in Taxonomy page

--- patterns/blog-grid.php
+++ patterns/blog-grid.php
@@ -16,13 +16,15 @@
 
 <!-- wp:query {"queryId":0,"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"displayLayout":{"type":"flex","columns":3},"align":"wide","layout":{"type":"constrained"}} -->
 <div class="wp-block-query alignwide"><!-- wp:post-template {"align":"wide"} -->
-<!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"253px","align":"wide"} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","bottom":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--small);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"400px","align":"wide"} /--></div>
+<!-- /wp:group -->
 
-<!-- wp:post-terms {"term":"category"} /-->
+<!-- wp:post-terms {"term":"category","className":"is-style-swt-post-terms-pill"} /-->
 
-<!-- wp:post-title {"level":4,"isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} /-->
+<!-- wp:post-title {"level":4,"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small","right":"0","bottom":"var:preset|spacing|x-small","left":"0"}}}} /-->
 
-<!-- wp:post-date {"isLink":true} /-->
+<!-- wp:post-date {"isLink":true,"textColor":"body"} /-->
 <!-- /wp:post-template -->
 
 <!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
