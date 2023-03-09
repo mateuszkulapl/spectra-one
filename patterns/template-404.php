082989ebc3a72dad12a95ae34ec0ddd99b24f5cc
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-03-09 15:38:31 +0530

    fix : error 404 page

--- patterns/template-404.php
+++ patterns/template-404.php
@@ -6,21 +6,16 @@
  * Inserter: false
  */
 ?>
-
-<!-- wp:template-part {"slug":"header","theme":"spectra","tagName":"header"} /-->
-
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><!-- wp:group {"tagName":"main","align":"wide","layout":{"type":"default"}} -->
 <main class="wp-block-group alignwide"><!-- wp:heading {"textAlign":"left","level":1,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|medium"}}},"className":"wp-block-heading"} -->
 <h1 class="wp-block-heading has-text-align-left" style="margin-bottom:var(--wp--preset--spacing--medium)">Error 404</h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"left","className":"aligncenter aligncenter"} -->
+<!-- wp:paragraph {"align":"left","className":"aligncenter"} -->
 <p class="has-text-align-left aligncenter">Unfortunately, this page doesn't exist.<br>Maybe try a search?</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:search {"label":"Search","showLabel":false,"width":300,"widthUnit":"px","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|md"}}},"className":"is-style-swt-search-minimal"} /--></main>
+<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search...","width":300,"widthUnit":"px","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|md"}}},"className":"is-style-swt-search-minimal"} /--></main>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
-
-<!-- wp:template-part {"slug":"footer","theme":"spectra","tagName":"footer"} /-->
