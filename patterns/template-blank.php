9794fbd9fa4d33cca703e3400a9bb135d84274d8
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-03-14 17:48:56 +0530

    fix : blank page issue

--- patterns/template-blank.php
+++ patterns/template-blank.php
@@ -6,6 +6,7 @@
  * Inserter: false
  */
 ?>
-<!-- wp:group {"tagName":"main","layout":{"type":"constrained"}} -->
-<!-- wp:post-content {"align":"full","layout":{"inherit":true}} /-->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:post-content {"align":"full","layout":{"type":"default"}} /--></div>
 <!-- /wp:group -->
