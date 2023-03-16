9019af3989233ad25c5189746aeba52e9b6ff5ac
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-03-16 15:50:47 +0530

    feat : added helper function for starter templates

--- functions.php
+++ functions.php
@@ -28,6 +28,7 @@ const SWT_DIR  = __DIR__ . SWT_DS;
  * Setup base spectra one functions
  */
 require_once SWT_DIR . 'inc/utilities/all.php';
+require_once SWT_DIR . 'inc/theme-options.php';
 require_once SWT_DIR . 'inc/scripts.php';
 require_once SWT_DIR . 'inc/blocks/all.php';
 require_once SWT_DIR . 'inc/extensions/all.php';
