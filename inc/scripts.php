69a56bf07c6418d2e14b7535e51e0b9afcfe4e7e
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-03-24 15:25:06 +0530

    refactor : moved language to another setup function

--- inc/scripts.php
+++ inc/scripts.php
@@ -115,7 +115,6 @@ function localize_editor_script() {
 	);
 }
 
-
 /**
  * Enqueue Editor Scripts.
  *
@@ -127,14 +126,6 @@ function enqueue_editor_block_styles() {
 
 	// Disable Core Block Patterns.
 	remove_theme_support( 'core-block-patterns' );
-
-	/*
-	* Make theme available for translation.
-	* Translations can be filed at WordPress.org. See: https://translate.wordpress.org/projects/wp-themes/twentyfifteen
-	* If you're building a theme based on spectra-one, use a find and replace
-	* to change 'spectra-one' to the name of your theme in all the template files
-	*/
-	load_theme_textdomain( 'spectra-one', get_uri() . 'languages' );
 
 	$file_prefix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 	$dir_name    = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? 'unminified' : 'minified';
@@ -150,3 +141,22 @@ function enqueue_editor_block_styles() {
 }
 
 add_action( 'after_setup_theme', SWT_NS . 'enqueue_editor_block_styles' );
+
+/**
+ * Enqueue Editor Scripts.
+ *
+ * @since 0.0.3
+ *
+ * @return void
+ */
+function spectra_one_setup() {
+	/*
+	* Make theme available for translation.
+	* Translations can be filed at WordPress.org. See: https://translate.wordpress.org/projects/wp-themes/twentyfifteen
+	* If you're building a theme based on spectra-one, use a find and replace
+	* to change 'spectra-one' to the name of your theme in all the template files
+	*/
+	load_theme_textdomain( 'spectra-one', get_uri() . 'languages' );
+}
+
+add_action( 'after_setup_theme', SWT_NS . 'spectra_one_setup' );
