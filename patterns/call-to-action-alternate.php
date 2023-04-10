cad1bf2aecfaefd2b1d78a8840c5190bb45ddf43
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-04-10 16:23:28 +0530

    fix : backward issues

---
+++ patterns/call-to-action-alternate.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Call to action alternate
+ * Slug: spectra-one/call-to-action-alternate
+ * Categories: banner
+ * Keywords: Call to action alternate
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"}}},"backgroundColor":"heading","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-heading-background-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:heading {"textAlign":"center","style":{"typography":{"letterSpacing":"-1px"}},"textColor":"background","className":"wp-block-heading"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color" style="letter-spacing:-1px"><?php esc_html_e( 'Build your own website', 'spectra-one' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color"><?php esc_html_e( 'Take your website to the next level!', 'spectra-one' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Get Started', 'spectra-one' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
