dfe93cc093c71a1649248eb22d65d2d28980502a
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-04-27 13:40:01 +0530

    fix : padding top spacing

--- patterns/template-page-sidebar.php
+++ patterns/template-page-sidebar.php
@@ -7,8 +7,8 @@
  */
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:columns {"align":"wide"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-large)"><!-- wp:columns {"align":"wide"} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"75%"} -->
 <div class="wp-block-column" style="flex-basis:75%"><!-- wp:post-title /-->
 <!-- wp:post-content /--></div>
