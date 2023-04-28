4c98c66680914927f2384cf70a21dccedf213277
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-04-28 11:27:03 +0530

    chor : fix phpcs issues , updated readme and ran pot command

--- inc/utilities/helpers.php
+++ inc/utilities/helpers.php
@@ -145,8 +145,8 @@ function render_image_placeholder( string $html, array $block ): string {
 		return $html;
 	}
 
-	$url_rel             = ( isset( $block['linkTarget'] ) && $block['linkTarget'] ) ? 'target="' . $block['linkTarget'] . '"' : '';
-	$url_target          = ( isset( $block['rel'] ) && $block['rel'] ) ? 'rel="' . $block['rel'] . '"' : '';
+	$url_rel    = ( isset( $block['linkTarget'] ) && $block['linkTarget'] ) ? 'target="' . $block['linkTarget'] . '"' : '';
+	$url_target = ( isset( $block['rel'] ) && $block['rel'] ) ? 'rel="' . $block['rel'] . '"' : '';
 
 	/** @psalm-suppress PossiblyFalseOperand */ // phpcs:ignore PossiblyFalseArgument, Generic.Commenting.DocComment.MissingShort
 	$default_placeholder = ( isset( $block['isLink'] ) && true === $block['isLink'] ) ? '<a href="' . get_the_permalink() . '" ' . $url_rel . ' ' . $url_target . ' ><figure class="wp-block-image"><img src="" alt=""/></figure></a>' : '<figure class="wp-block-image"><img src="" alt=""/></figure>'; 
