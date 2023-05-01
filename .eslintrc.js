eb53d77e4fe1c2c298f7d33fdf35c68028b4fbf0
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-05-01 12:33:02 +0530

    chor : new theme setup changes

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
+	rules: {
+		camelcase: 'off',
+		'no-console': 'off',
+		'no-alert': 'off',
+		'space-before-function-paren': 'off',
+		'no-mixed-spaces-and-tabs': 'off',
+	},
+	parserOptions: {
+		requireConfigFile: false,
+	},
+	globals: {
+		alert: true,
+		confirm: true,
+		jQuery: true,
+		module: true,
+		aist: true,
+		aistexport: true,
+		aist_dasboard: true,
+	},
+};
--- inc/admin/welcome-notice.php
+++ inc/admin/welcome-notice.php
@@ -178,7 +178,7 @@ function welcome_notice_display_conditions(): bool {
  */
 function is_spectra_plugin_status(): string {
 	$plugin_slug = 'ultimate-addons-for-gutenberg/ultimate-addons-for-gutenberg.php';
-	$status = 'not-installed';
+	$status      = 'not-installed';
 
 	if ( is_plugin_active( $plugin_slug ) ) {
 		return 'activated';
