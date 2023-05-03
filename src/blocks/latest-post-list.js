4df433b14a6c30a5ebf4514ed5912060007e9eb6
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-05-03 17:44:08 +0530

    chor : fix lint issues

--- src/blocks/latest-post-list.js
+++ src/blocks/latest-post-list.js
@@ -2,19 +2,19 @@ import { createHigherOrderComponent } from '@wordpress/compose';
 import { addFilter } from '@wordpress/hooks';
 
 const latestPostList = createHigherOrderComponent( ( BlockListBlock ) => {
-    return ( props ) => {
-        const { name, attributes } = props;
+	return ( props ) => {
+		const { name, attributes } = props;
 
-        if ( name !== 'core/latest-posts' ) {
-            return <BlockListBlock { ...props }/>;
-        }
+		if ( name !== 'core/latest-posts' ) {
+			return <BlockListBlock { ...props } />;
+		}
 
-        const { displayFeaturedImage } = attributes;
+		const { displayFeaturedImage } = attributes;
 
-        const customClass = displayFeaturedImage ? 'swt-has-featured-image' : '';
+		const customClass = displayFeaturedImage ? 'swt-has-featured-image' : '';
 
-        return <BlockListBlock { ...props } className={ customClass }/>;
-    };
+		return <BlockListBlock { ...props } className={ customClass } />;
+	};
 }, 'latestPostList' );
 
 addFilter(
