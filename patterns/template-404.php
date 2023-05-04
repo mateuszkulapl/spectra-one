c2abcc5e1f3a2e065b47b4c7494c152fa25fb02c
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-05-04 13:33:22 +0530

    feat : added revamped pages, post and sidebar

--- patterns/template-404.php
+++ patterns/template-404.php
@@ -10,13 +10,13 @@
 <!-- wp:group {"tagName":"main","align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|large","padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
 <main class="wp-block-group alignwide has-primary-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"bottom":"0"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="padding-bottom:0"><!-- wp:heading {"textAlign":"center","level":1,"align":"wide","textColor":"background","className":"wp-block-heading"} -->
-<h1 class="wp-block-heading alignwide has-text-align-center has-background-color has-text-color">Page Not Found</h1>
+<h1 class="wp-block-heading alignwide has-text-align-center has-background-color has-text-color"><?php esc_html_e( 'Page Not Found', 'spectra-one' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","textColor":"background","className":"aligncenter","fontSize":"large"} -->
-<p class="has-text-align-center aligncenter has-background-color has-text-color has-large-font-size">Can't find what you're looking for? Don't worry, we've got your back.</p>
+<p class="has-text-align-center aligncenter has-background-color has-text-color has-large-font-size"><?php esc_html_e( 'Can\'t find what you\'re looking for? Don\'t worry, we\'ve got your back.', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search...","width":50,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|md"}},"border":{"radius":"6px"}},"borderColor":"background","backgroundColor":"background","className":"is-style-swt-search-minimal"} /--></main>
+<!-- wp:search {"label":"<?php esc_html_e( 'Search', 'spectra-one' ); ?>","showLabel":false,"placeholder":"<?php esc_html_e( 'Search...', 'spectra-one' ); ?>","width":50,"widthUnit":"%","buttonText":"<?php esc_html_e( 'Search', 'spectra-one' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|md"}},"border":{"radius":"6px"}},"borderColor":"background","backgroundColor":"background","className":"is-style-swt-search-minimal"} /--></main>
 <!-- /wp:group -->
--- patterns/template-page-sidebar.php
+++ patterns/template-page-sidebar.php
@@ -6,16 +6,26 @@
  */
 ?>
 
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|large","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","className":"swt-block-post-banner-group","layout":{"type":"constrained"}} -->
+<div class="wp-block-group swt-block-post-banner-group has-primary-background-color has-background" style="margin-top:0;margin-bottom:0"><!-- wp:post-title {"textAlign":"center","level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|large"}}},"textColor":"background"} /--></div>
+<!-- /wp:group -->
+
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-large)"><!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"75%"} -->
-<div class="wp-block-column" style="flex-basis:75%"><!-- wp:post-title /-->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"75%","style":{"spacing":{"padding":{"right":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--small);flex-basis:75%"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|medium"},"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--medium)"><!-- wp:post-featured-image /-->
+
 <!-- wp:post-content /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium"}}},"backgroundColor":"surface","className":"swt-block-comment-group","layout":{"type":"constrained"}} -->
+<div class="wp-block-group swt-block-comment-group has-surface-background-color has-background" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:post-comments-form /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:column -->
-<!-- wp:column {"width":"25%"} -->
-<div class="wp-block-column" style="flex-basis:25%">
-<!-- wp:template-part {"slug":"sidebar","theme":"spectra-one"} /-->
-</div>
+
+<!-- wp:column {"width":"25%","style":{"spacing":{"padding":{"left":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-column" style="padding-left:var(--wp--preset--spacing--small);flex-basis:25%"><!-- wp:template-part {"slug":"sidebar","theme":"spectra-one"} /--></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
