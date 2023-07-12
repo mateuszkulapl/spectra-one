6b9810f12d383cf04d0962c6b374f34ddd5912d0
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-07-12 17:50:17 +0530

    fix : lint css and js

--- assets/js/editor.asset.php
+++ assets/js/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-edit-site', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '5c9a3944bdd4330fdab8');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-edit-site', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'd86a53aef09785923469');
--- src/admin/spectra-editor.js
+++ src/admin/spectra-editor.js
@@ -4,72 +4,63 @@ import {
 	installPlugin,
 } from '../components/plugin-helpers.js';
 
-
 const {
-    activating,
-    installing,
-    done,
-    activationUrl,
-    pluginSlug,
-    pluginStatus,
+	activating,
+	installing,
+	done,
+	activationUrl,
+	pluginSlug,
+	pluginStatus,
 } = spectraOne;
 
-const activateSpectra = async (target) => {
+const activateSpectra = async ( target ) => {
+	target.textContent = activating;
+	await activatePluginUrl( activationUrl );
 
-    console.log(target);
+	target.classList.remove( 'updating-message' );
+	target.classList.add( 'updated-message' );
 
-    target.textContent = activating;
-    await activatePluginUrl( activationUrl );
-
-    target.classList.remove( 'updating-message' );
-    target.classList.add( 'updated-message' );
-
-    target.textContent = done;
+	target.textContent = done;
 };
 
-document.addEventListener('click', async (e) => {
-    let elementClass = e?.target?.className ? e.target.className : '';
+document.addEventListener( 'click', async ( e ) => {
+	const elementClass = e?.target?.className ? e.target.className : '';
 
-    if (elementClass && ( typeof elementClass === 'string' ) && elementClass.includes( 'block-editor-inserter__patterns-explore-button' )) {
+	if ( elementClass && ( typeof elementClass === 'string' ) && elementClass.includes( 'block-editor-inserter__patterns-explore-button' ) ) {
+		const button = document.createElement( 'button' );
+		button.textContent = __( 'Get more blocks with Spectra', 'spectra-one' );
+		button.setAttribute( 'class', 'swt-template-kit-spectra components-button is-primary' );
 
+		const container = document.querySelector( '.block-editor-block-patterns-explorer__sidebar__categories-list' );
 
-        const button = document.createElement('button');
-        button.textContent = __( 'Get more blocks with Spectra','spectra-one');
-        button.setAttribute("class", "swt-template-kit-spectra components-button is-primary");
+		if ( container && ! container.querySelector( '.swt-template-kit-spectra' ) ) {
+			container.appendChild( button );
+		}
+	}
 
-        const container = document.querySelector('.block-editor-block-patterns-explorer__sidebar__categories-list');
+	if ( elementClass && ( typeof elementClass === 'string' ) && elementClass.includes( 'swt-template-kit-spectra' ) ) {
+		const target = document.querySelector( '#ast-block-templates-button' );
 
-        if( container && ! container.querySelector('.swt-template-kit-spectra') ) {
-            container.appendChild( button);
-        }
-    }
+		if ( pluginStatus === 'not-installed' ) {
+			e.target.classList.add( 'updating-message' );
+			e.target.classList.add( 'disabled' );
+			e.target.textContent = installing;
+			await installPlugin( pluginSlug );
+			await activateSpectra( e.target );
+			location.reload();
+		}
 
-    if (elementClass && ( typeof elementClass === 'string' ) && elementClass.includes( 'swt-template-kit-spectra' )) {
-        const target = document.querySelector('#ast-block-templates-button');
+		if ( pluginStatus === 'installed' ) {
+			e.target.classList.add( 'updating-message' );
+			e.target.classList.add( 'disabled' );
+			await activateSpectra( e.target );
+			location.reload();
+		}
 
-        console.log(pluginStatus);
-
-        if( pluginStatus === 'not-installed' ) {
-            e.target.classList.add( 'updating-message' );
-            e.target.classList.add( 'disabled' );
-            e.target.textContent = installing;
-            await installPlugin( pluginSlug );
-            await activateSpectra(e.target);
-            location.reload();
-        }
-
-        if ( pluginStatus === 'installed' ) {
-            e.target.classList.add( 'updating-message' );
-            e.target.classList.add( 'disabled' );
-            await activateSpectra(e.target);
-            location.reload();
-        }
-
-        if( target ) {
-
-            if( pluginStatus === 'activated' ) {
-                target.click();
-            }
-        }
-    }
-});
+		if ( target ) {
+			if ( pluginStatus === 'activated' ) {
+				target.click();
+			}
+		}
+	}
+} );
