23bc8eb13ad524731c9a9ff04216898a3084c628
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-07-14 14:32:45 +0530

    feat : added featured patterns

--- patterns/feature-6.php
+++ patterns/feature-6.php
@@ -5,32 +5,20 @@
  * Categories: services
  * Keywords: three column featured
  */
-
-$get_url = trailingslashit(get_template_directory_uri());
-
-$images = [
-    $get_url . "assets/image/featured-1.svg",
-    $get_url . "assets/image/featured-2.svg",
-    $get_url . "assets/image/featured-3.svg",
-    $get_url . "assets/image/featured-4.svg",
-    $get_url . "assets/image/featured-5.svg",
-    $get_url . "assets/image/featured-6.svg",
-];
-
 ?>
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"},"blockGap":"var:preset|spacing|large"},"border":{"radius":"6px"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="border-radius:6px;padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|x-small"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide" style="padding-bottom:var(--wp--preset--spacing--x-small)"><!-- wp:paragraph {"align":"center","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"textColor":"primary","fontSize":"x-small"} -->
-<p class="has-text-align-center has-primary-color has-text-color has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">Full Site Editing</p>
+<p class="has-text-align-center has-primary-color has-text-color has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase"><?php esc_html_e( 'Full Site Editing', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"textAlign":"center"} -->
-<h2 class="wp-block-heading has-text-align-center">Unleash the power of block editor</h2>
+<h2 class="wp-block-heading has-text-align-center"><?php esc_html_e( 'Unleash the power of block editor', 'spectra-one' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">Get online quickly with easy-to-use Full Site Editor builder</p>
+<p class="has-text-align-center"><?php esc_html_e( 'Get online quickly with easy-to-use Full Site Editor builder', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
@@ -39,19 +27,19 @@ $images = [
 <div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
-<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">01</p>
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase"><?php esc_html_e( '01', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
-<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Easily Customizable</h5>
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)"><?php esc_html_e( 'Easily Customizable', 'spectra-one' ); ?></h5>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Tailor your website to your liking with endless options to personalize the design and functionality.</p>
+<p><?php esc_html_e( 'Tailor your website to your liking with endless options to personalize the design and functionality.', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"textColor":"primary"} -->
-<p class="has-primary-color has-text-color">Learn More →</p>
+<p class="has-primary-color has-text-color"><a href="#"><?php esc_html_e( 'Learn More →', 'spectra-one' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
@@ -59,19 +47,19 @@ $images = [
 <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
-<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">02</p>
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase"><?php esc_html_e( '02', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
-<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Blazing Fast</h5>
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)"><?php esc_html_e( 'Blazing Fast', 'spectra-one' ); ?></h5>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Experience lightning-fast page load speeds for optimal user experience and SEO ranking.</p>
+<p><?php esc_html_e( 'Experience lightning-fast page load speeds for optimal user experience and SEO ranking.', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"textColor":"primary"} -->
-<p class="has-primary-color has-text-color">Learn More →</p>
+<p class="has-primary-color has-text-color"><a href="#"><?php esc_html_e( 'Learn More →', 'spectra-one' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
@@ -79,19 +67,19 @@ $images = [
 <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
-<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">03</p>
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase"><?php esc_html_e( '03', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
-<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">SEO Optimized</h5>
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)"><?php esc_html_e( 'SEO Optimized', 'spectra-one' ); ?></h5>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Built with search engines in mind, achieve higher rankings and more traffic with optimized code.</p>
+<p><?php esc_html_e( 'Built with search engines in mind, achieve higher rankings and more traffic with optimized code.', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"textColor":"primary"} -->
-<p class="has-primary-color has-text-color">Learn More →</p>
+<p class="has-primary-color has-text-color"><a href="#"><?php esc_html_e( 'Learn More →', 'spectra-one' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
@@ -101,19 +89,19 @@ $images = [
 <div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
-<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">04</p>
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase"><?php esc_html_e( '04', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
-<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Drag and Drop</h5>
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)"><?php esc_html_e( 'Drag and Drop', 'spectra-one' ); ?></h5>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Easily customize your website's layout and design using an intuitive, drag-and-drop interface.</p>
+<p><?php esc_html_e( 'Easily customize your website\'s layout and design using an intuitive, drag-and-drop interface.', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"textColor":"primary"} -->
-<p class="has-primary-color has-text-color">Learn More →</p>
+<p class="has-primary-color has-text-color"><a href="#"><?php esc_html_e( 'Learn More →', 'spectra-one' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
@@ -121,19 +109,19 @@ $images = [
 <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
-<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">05</p>
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase"><?php esc_html_e( '05', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
-<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Global Styles</h5>
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)"><?php esc_html_e( 'Global Styles', 'spectra-one' ); ?></h5>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Define global styling settings for your website, such as typography, colors, and spacing.</p>
+<p><?php esc_html_e( 'Define global styling settings for your website, such as typography, colors, and spacing.', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"textColor":"primary"} -->
-<p class="has-primary-color has-text-color">Learn More →</p>
+<p class="has-primary-color has-text-color"><a href="#"><?php esc_html_e( 'Learn More →', 'spectra-one' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
@@ -141,19 +129,19 @@ $images = [
 <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
 <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
-<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">06</p>
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase"><?php esc_html_e( '06', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
-<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Design Patterns</h5>
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)"><?php esc_html_e( 'Design Patterns', 'spectra-one' ); ?></h5>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>Spectra offers a wide selection of meticulously crafted patterns to enhance your content.</p>
+<p><?php esc_html_e( 'Spectra offers a wide selection of meticulously crafted patterns to enhance your content.', 'spectra-one' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"textColor":"primary"} -->
-<p class="has-primary-color has-text-color">Learn More →</p>
+<p class="has-primary-color has-text-color"><a href="#"><?php esc_html_e( 'Learn More →', 'spectra-one' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
