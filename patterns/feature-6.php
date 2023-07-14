bc85eb40bbd305482b6bf3b0966707b8d26f02bf
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-07-14 14:18:40 +0530

    feat : added featured patterns

---
+++ patterns/feature-6.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Title: Three column featured
+ * Slug: spectra-one/feature-6
+ * Categories: services
+ * Keywords: three column featured
+ */
+
+$get_url = trailingslashit(get_template_directory_uri());
+
+$images = [
+    $get_url . "assets/image/featured-1.svg",
+    $get_url . "assets/image/featured-2.svg",
+    $get_url . "assets/image/featured-3.svg",
+    $get_url . "assets/image/featured-4.svg",
+    $get_url . "assets/image/featured-5.svg",
+    $get_url . "assets/image/featured-6.svg",
+];
+
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"},"blockGap":"var:preset|spacing|large"},"border":{"radius":"6px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-radius:6px;padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|x-small"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-bottom:var(--wp--preset--spacing--x-small)"><!-- wp:paragraph {"align":"center","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"textColor":"primary","fontSize":"x-small"} -->
+<p class="has-text-align-center has-primary-color has-text-color has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">Full Site Editing</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Unleash the power of block editor</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Get online quickly with easy-to-use Full Site Editor builder</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">01</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Easily Customizable</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Tailor your website to your liking with endless options to personalize the design and functionality.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"primary"} -->
+<p class="has-primary-color has-text-color">Learn More →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">02</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Blazing Fast</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Experience lightning-fast page load speeds for optimal user experience and SEO ranking.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"primary"} -->
+<p class="has-primary-color has-text-color">Learn More →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">03</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">SEO Optimized</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Built with search engines in mind, achieve higher rankings and more traffic with optimized code.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"primary"} -->
+<p class="has-primary-color has-text-color">Learn More →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">04</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Drag and Drop</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Easily customize your website's layout and design using an intuitive, drag-and-drop interface.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"primary"} -->
+<p class="has-primary-color has-text-color">Learn More →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">05</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Global Styles</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Define global styling settings for your website, such as typography, colors, and spacing.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"primary"} -->
+<p class="has-primary-color has-text-color">Learn More →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xx-small"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}},"border":{"radius":"6px","width":"1px"},"dimensions":{"minHeight":"100%"}},"borderColor":"surface","backgroundColor":"surface","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-border-color has-surface-border-color has-surface-background-color has-background" style="border-width:1px;border-radius:6px;min-height:100%;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"align":"left","style":{"typography":{"letterSpacing":"2px","textTransform":"uppercase"}},"fontSize":"x-small"} -->
+<p class="has-text-align-left has-x-small-font-size" style="letter-spacing:2px;text-transform:uppercase">06</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}},"textColor":"heading","className":"wp-block-heading"} -->
+<h5 class="wp-block-heading has-heading-color has-text-color" style="margin-top:var(--wp--preset--spacing--x-small)">Design Patterns</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Spectra offers a wide selection of meticulously crafted patterns to enhance your content.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"primary"} -->
+<p class="has-primary-color has-text-color">Learn More →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
