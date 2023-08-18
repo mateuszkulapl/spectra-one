5d11a06c98ce65a1e3b3e2d74d1f5b2065f90025
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-08-18 16:00:04 +0530

    fix : theme updater function

--- inc/theme-updater.php
+++ inc/theme-updater.php
@@ -25,10 +25,10 @@ add_action( 'after_setup_theme', SWT_NS . 'run_function_after_theme_update' );
  * @since x.x.x
  */
 function run_function_after_theme_update():void {
-	$current_version = wp_get_theme()->get( 'Version' );
-	$old_version     = get_option( 'swt_theme_version' );
+	list( $current_version ) = explode( '-', wp_get_theme()->get( 'Version' ) );
+	$old_version             = get_option( 'swt_theme_version' );
 
-	if ( $old_version !== $current_version ) {
+	if ( $old_version !== $current_version && $old_version < $current_version ) {
 		// Run your function here.
 		if ( is_spectra_plugin() ) {
 			$uagb_fonts = new \UAGB_FSE_Fonts_Compatibility();
